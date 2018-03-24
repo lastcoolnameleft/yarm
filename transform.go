2f24923ddb4ac0043c0a42978dda4f285959d570
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-03-23 19:35:02 -0500

    add test content files

--- transform.go
+++ transform.go
@@ -10,9 +10,15 @@ import (
 
 // Transform template + values => string
 func Transform(content []byte, values Values) (res string, err error) {
+	println("----Content----")
+	println(string(content))
+	println("----Values----")
+	fmt.Printf("%+v\n", values)
 	tmpl, err := template.New("test").Parse(string(content))
 	var tpl bytes.Buffer
 	err = tmpl.Execute(&tpl, values)
+	println("----Post Template----")
+	println(tpl.String())
 
 	j2, err := yaml.YAMLToJSON(tpl.Bytes())
 	if err != nil {
